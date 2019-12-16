56bafd959d242f0709c25f734c9bf7eecb70fd45
ry-na <ryo.natori0809@gmail.com>
2019-12-17 00:16:05 +1300

    add reload button

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-$.get(
-  `https://script.google.com/macros/s/AKfycbzMTMnJlks-ESlCVqmtHsZWLsxvMgdF7HsLsYB2lyhMzLnj9SJ5/exec`
-).done(function(data) {
-  $(".talk_theme").text(data);
-});
+function load() {
+  $.get(
+    `https://script.google.com/macros/s/AKfycbzMTMnJlks-ESlCVqmtHsZWLsxvMgdF7HsLsYB2lyhMzLnj9SJ5/exec`
+  ).done(function(data) {
+    $(".talk_theme").text(data);
+  });
+}
+load()
