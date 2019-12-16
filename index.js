560f0c1dd0aa9daa2ebe6fbd36f922d702a07f9a
ry-na <ryo.natori0809@gmail.com>
2019-12-17 00:32:34 +1300

    メンバーデータを読み込むようにした

--- index.js
+++ index.js
@@ -4,5 +4,18 @@ function load() {
   ).done(function(data) {
     $(".talk_theme").text(data);
   });
+  $.get(
+    `https://script.google.com/macros/s/AKfycby-S_ncR_NABga_kHjkSNnIPpr7CyvKiBuhFcPZ1h5rHFO08mY/exec`
+  ).done(function(groups) {
+    $(".group_area").empty();
+    for (let [i, members] of groups.entries()) {
+      $(".group_area").append(`<div class="group_num">${i + 1}</div>`);
+      let memberDom = "";
+      for (let member of members) {
+        memberDom += `<div class="group_member_name">${member}</div>`;
+      }
+      $(".group_area").append(`<div class="group_member">${memberDom}</div>`);
+    }
+  });
 }
-load()
+load();
